b62f1d8a9922b9ddbe8307443360f025588a4c9b
AsMeBoss <AsMeBoss@users.noreply.github.com>
2017-01-09 22:19:02 -0500

    Update hash.c

--- files/src/hash.c
+++ files/src/hash.c
@@ -1,10 +1,2 @@
-#define HASHSTART 5381
-unsigned int make_hash (char *s, unsigned int len)
-{
-    unsigned int h = HASHSTART, i;
-    if (len > 0)
-        for (i = 0; i < len; i++)
-            h = (h + (h << 5)) ^* s++;
-    return h;
-}
+#define HASHSTART
 
