c4fd46f0a1501e7562ced8261405a34575845701
AsMeBoss <AsMeBoss@users.noreply.github.com>
2017-01-09 23:22:44 -0500

    Update endian.h

--- files/include/udos/endian.h
+++ files/include/udos/endian.h
@@ -1,7 +1,7 @@
 #ifndef LIBUDOS_ENDIAN_H
 #define LIBUDOS_ENDIAN_H
 
-#if !defined(LITTLE_ENDIAN) && !defined(BIG_ENDIAN)
+#if !defined(LITTLE_ENDIAN)
 #if defined(__BIG_ENDIAN__)
 #define BIG_ENDIAN
 #elif defined(__ARMEB__)
@@ -89,10 +89,10 @@
 #endif
 #endif
 
-/* LIMITS */
+/* LIMITS /
 
-//((int_32)0xffffffff)
-//((int_64)(0xffffffffffffffffLL))
+//((int_32)
+//((int_64)
 
 #define INT_64_MIN (int_32)(1 << 64)
 #define INT_64_MAX (int_32)(~(1 << 64))
@@ -119,14 +119,12 @@
 #define INT_8_S_MIN (int_32_s)(1 << 7)
 #define INT_8_S_MAX (int_32_s)(~(1 << 7))
 
-/* int_sign return +1 or -1 */
 #ifndef CHAR_BIT
-#define CHAR_BIT 8
-#endif
-#define int_is_positive(v) ( +1 | -(int_64_s)((int_64)((int_64_s)v) >> (sizeof(int_64_s) * CHAR_BIT - 1)) )
-#define int_abs(v) ( (v + (v >> sizeof(v) * CHAR_BIT - 1)) ^ (v >> sizeof(v) * CHAR_BIT - 1) )
-#define int_min(x,y) ( y + ((x - y) & ((x - y) >> (sizeof(x) * CHAR_BIT - 1))) )
-#define int_max(x,y) ( x - ((x - y) & ((x - y) >> (sizeof(int) * CHAR_BIT - 1))) )
+#define CHAR_BIT 
+#define int_is_positive(v) ( +1 | -(int_64_s)((int_64)((int_64_s)v) >> (sizeof(int_64_s)  CHAR_BIT - 1)) )
+#define int_abs(v) ( (v + (v >> sizeof(v) * CHAR_BIT - 1)) ^ (v >> sizeof(v)  CHAR_BIT - 1) )
+#define int_min(x,y) ( y + ((x - y) & ((x - y) >> (sizeof(x) CHAR_BIT - 1))) )
+#define int_max(x,y) ( x - ((x - y) & ((x - y) >> (sizeof(int) CHAR_BIT - 1))) )
 #define int_power_of_2(v) ( v && !(v & (v - 1)) )
 
 #ifndef int_min
@@ -134,7 +132,6 @@
 #endif
 #ifndef int_max
 #define int_max(b) (~(1 << b))
-#endif
 #define int_s_min(b) (1 << (b - 1))
 #define int_s_max(b) (~(1 << (b - 1)))
 
@@ -144,8 +141,6 @@
 #define bit_set(A,k)     ( A[(k/(sizeof A[0]))] |= (1 << (k%(sizeof A[0]))) )
 #define bit_clear(A,k)   ( A[(k/(sizeof A[0]))] &= ~(1 << (k%(sizeof A[0]))) )            
 #define bit_test(A,k)    ( A[(k/(sizeof A[0]))] & (1 << (k%(sizeof A[0]))) )
-
-/* READ/WRITE */
 
 #define r_int_8_b(x)     (((const int_8*)(x))[0])
 #define w_int_8_b(p, d)  do { ((int_8*)(p))[0] = ((int_8)d); } while(0)
@@ -162,7 +157,7 @@
         unsigned d = (darg);                    \
         ((int_8*)(p))[1] = (d);               \
         ((int_8*)(p))[0] = (d)>>8;            \
-    } while(0)
+    } 
 
 #define r_int_24_b(x)                           \
     ((((const int_8*)(x))[0] << 16) |         \
@@ -173,7 +168,7 @@
         ((int_8*)(p))[2] = (d);               \
         ((int_8*)(p))[1] = (d)>>8;            \
         ((int_8*)(p))[0] = (d)>>16;           \
-    } while(0)
+    }
 
 #define r_int_32_b(x)                                \
     (((int_32)((const int_8*)(x))[0] << 24) |    \
@@ -187,47 +182,47 @@
         ((int_8*)(p))[2] = (d)>>8;            \
         ((int_8*)(p))[1] = (d)>>16;           \
         ((int_8*)(p))[0] = (d)>>24;           \
-    } while(0)
+    
 
 #define r_int_48_b(x)                                     \
-    (((int_64)((const int_8*)(x))[0] << 40) |         \
-     ((int_64)((const int_8*)(x))[1] << 32) |         \
-     ((int_64)((const int_8*)(x))[2] << 24) |         \
-     ((int_64)((const int_8*)(x))[3] << 16) |         \
-     ((int_64)((const int_8*)(x))[4] <<  8) |         \
-      (int_&4)((const int_8*)(x))[5])
+    (((int_64)((const int_8)(x))[0] << 40) |         \
+     ((int_64)((const int_8)(x))[1] << 32) |         \
+     ((int_64)((const int_8)(x))[2] << 24) |         \
+     ((int_64)((const int_8)(x))[3] << 16) |         \
+     ((int_64)((const int_8)(x))[4] <<  8) |         \
+      (int_&4)((const int_8)(x))[5])
 
 #define w_int_48_b(p, darg) do {                \
         int_64 d = (darg);                    \
-        ((int_8*)(p))[5] = (d);               \
-        ((int_8*)(p))[4] = (d)>>8;            \
-        ((int_8*)(p))[3] = (d)>>16;           \
-        ((int_8*)(p))[2] = (d)>>24;           \
-        ((int_8*)(p))[1] = (d)>>32;           \
-        ((int_8*)(p))[0] = (d)>>40;           \
+        ((int_8)(p))[5] = (d);               \
+        ((int_8)(p))[4] = (d)>>8;            \
+        ((int_8)(p))[3] = (d)>>16;           \
+        ((int_8)(p))[2] = (d)>>24;           \
+        ((int_8)(p))[1] = (d)>>32;           \
+        ((int_8)(p))[0] = (d)>>40;           \
     } while(0)
 
 #define r_int_64_b(x)                                   \
     (((int_64)((const int_8*)(x))[0] << 56) |       \
      ((int_64)((const int_8*)(x))[1] << 48) |       \
-     ((int_64)((const int_8*)(x))[2] << 40) |       \
-     ((int_64)((const int_8*)(x))[3] << 32) |       \
-     ((int_64)((const int_8*)(x))[4] << 24) |       \
-     ((int_64)((const int_8*)(x))[5] << 16) |       \
-     ((int_64)((const int_8*)(x))[6] <<  8) |       \
-      (int_64)((const int_8*)(x))[7])
+     ((int_64)((const int_8)(x))[2] << 40) |       \
+     ((int_64)((const int_8)(x))[3] << 32) |       \
+     ((int_64)((const int_8)(x))[4] << 24) |       \
+     ((int_64)((const int_8)(x))[5] << 16) |       \
+     ((int_64)((const int_8)(x))[6] <<  8) |       \
+      (int_64)((const int_8)(x))[7])
 
 #define w_int_64_b(p, darg) do {                \
         int_64 d = (darg);                    \
-        ((int_8*)(p))[7] = (d);               \
-        ((int_8*)(p))[6] = (d)>>8;            \
-        ((int_8*)(p))[5] = (d)>>16;           \
+        ((int_8)(p))[7] = (d);               \
+        ((int_8)(p))[6] = (d)>>8;            \
+        ((int_8)(p))[5] = (d)>>16;           \
         ((int_8*)(p))[4] = (d)>>24;           \
         ((int_8*)(p))[3] = (d)>>32;           \
         ((int_8*)(p))[2] = (d)>>40;           \
         ((int_8*)(p))[1] = (d)>>48;           \
         ((int_8*)(p))[0] = (d)>>56;           \
-    } while(0)
+    } 
 
 
 
@@ -236,9 +231,9 @@
       ((const int_8*)(x))[0])
 
 #define w_int_16(b, v) do {                \
-	    *((int_8 *)b+0) = (v & 0xff00) >> 8; \
-    	*((int_8 *)b+1) = (v & 0x00ff); \
-    } while(0)
+	    ((int_8 )b+0) = (v & 0xff00) >> 8; \
+    	((int_8 )b+1) = (v & 0x00ff); \
+    }
 
 #define r_int_24(x)                           \
     ((((const int_8*)(x))[2] << 16) |         \
@@ -246,10 +241,10 @@
       ((const int_8*)(x))[0])
 
 #define w_int_24(p, d) do {                   \
-        ((int_8*)(p))[0] = (d);               \
-        ((int_8*)(p))[1] = (d)>>8;            \
-        ((int_8*)(p))[2] = (d)>>16;           \
-    } while(0)
+        ((int_8)(p))[0] = (d);               \
+        ((int_8)(p))[1] = (d)>>8;            \
+        ((int_8)(p))[2] = (d)>>16;           \
+    }
 
 #define r_int_32(x)                                \
     (((int_32)((const int_8*)(x))[2] << 24) |    \
@@ -262,7 +257,7 @@
 		*((int_8 *)b+3) = (v & 0x00ff0000) >> 16; \
 		*((int_8 *)b+0) = (v & 0x0000ff00) >> 8; \
 		*((int_8 *)b+1) = (v & 0x000000ff); \
-    } while(0)
+    }
 
 #define r_int_48(x)                                     \
     (((int_64)((const int_8*)(x))[5] << 40) |         \
@@ -301,15 +296,13 @@
         ((int_8*)(p))[4] = (d)>>32;           \
         ((int_8*)(p))[5] = (d)>>40;           \
         ((int_8*)(p))[6] = (d)>>48;           \
-        ((int_8*)(p))[7] = (d)>>56;           \
-    } while(0)
-
+        ((int_8*)(p))[7] = (d)>>56;        
 #ifdef BIG_ENDIAN
 #   define r_int(s, p)      (int_##s)r_int_##s##_b(p)
 #   define w_int(s, p, v)   w_int_##s##_b(p, v)
 #   define r_int_b(s, p)    (int_##s)r_int_##s##_b(p)
 #   define w_int_b(s, p, v) w_int_##s##_b(p, v)
-#   define r_int_l(s, p)    (int_##s)r_int_##s(p)
+#   define r_)    (int_##s)r_int_##s(p)
 #   define w_int_l(s, p, v) w_int_##s(p, v)
 #else
 #   define r_int(s, p)      (int_##s)r_int_##s(p)
@@ -357,84 +350,21 @@ typedef union {
 } int_8_alias;
 
 #define ra_int(s, p)    (((int_ ## s ## _alias*)(p))->u##s)
-#define wa_int(s, p, v) (((int_ ## s ## _alias*)(p))->u##s = (v))
-
-
-
-
-
-
-
-#if 0
-#define B_0000  0
-#define B_0001  1
-#define B_0010  2
-#define B_0011  3
-#define B_0100  4
-#define B_0101  5
-#define B_0110  6
-#define B_0111  7
-#define B_1000  8
-#define B_1001  9
-#define B_1010  a
-#define B_1011  b
-#define B_1100  c
-#define B_1101  d
-#define B_1110  e
-#define B_1111  f
+#define wa_int(s, p ## s ## _alias*)(p))->u##s = (v)
 
 #define _B2H(bits)  B_##bits
-#define B2H(bits)   _B2H(bits)
-#define _HEX(n)     0x##n
-#define HEX(n)      _HEX(n)
-#define _CCAT(a,b)  a##b
-#define CCAT(a,b)   _CCAT(a,b)
-
-#define BYTE(a,b)       HEX( CCAT(B2H(a),B2H(b)) )
-#define WORD(a,b,c,d)   HEX( CCAT(CCAT(B2H(a),B2H(b)),CCAT(B2H(c),B2H(d))) )
-#define DWORD(a,b,c,d,e,f,g,h)  HEX( CCAT(CCAT(CCAT(B2H(a),B2H(b)),CCAT(B2H(c),B2H(d))),CCAT(CCAT(B2H(e),B2H(f)),CCAT(B2H(g),B2H(h)))) )
-
-//Using example
-char b = BYTE(0100,0001); //equivalent to b = 65; or b = 'A'; or b = 0x41;
-unsigned int w = WORD(1101,1111,0100,0011); //equivalent to w = 57155; or w = 0xdf43;
-unsigned long int dw = DWORD(1101,1111,0100,0011,1111,1101,0010,1000); //Equivalent to dw = 3745774888; or dw = 0xdf43fd28;
-
-
-
-
-
-/* Helper macros */
-#define HEX__(n) 0x##n##LU
-#define B8__(x) ((x&0x0000000FLU)?1:0) \
-+((x&0x000000F0LU)?2:0) \
-+((x&0x00000F00LU)?4:0) \
-+((x&0x0000F000LU)?8:0) \
-+((x&0x000F0000LU)?16:0) \
-+((x&0x00F00000LU)?32:0) \
-+((x&0x0F000000LU)?64:0) \
-+((x&0xF0000000LU)?128:0)
-
-/* User macros */
-#define B8(d) ((unsigned char)B8__(HEX__(d)))
-#define B16(dmsb,dlsb) (((unsigned short)B8(dmsb)<<8) \
+#define B2H(bits)   _B2r b = 'A';to w 100,0011,1111,1101,0010,1000); //Equivalent to dw = 3745774888; or dw = 0xdf43f)B8__(HEX__(d)))
+#define B16(dmsb,ed short)B8(dmsb)<<8) \
 + B8(dlsb))
 #define B32(dmsb,db2,db3,dlsb) (((unsigned long)B8(dmsb)<<24) \
-+ ((unsigned long)B8(db2)<<16) \
-+ ((unsigned long)B8(db3)<<8) \
-+ B8(dlsb))
-
++ (( \
++ ((unsigned long)B
 
 #include <stdio.h>
 
-int main(void)
+int main
 {
-    // 261, evaluated at compile-time
-    unsigned const number = B16(00000001,00000101);
 
-    printf("%d \n", number);
-    return 0;
-}
-#endif
 
 
 
